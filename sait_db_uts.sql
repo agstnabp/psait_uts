5b6a258b57a81dc881ad6a79e35f0883b2bbffd2
agstnabp <118712819+agstnabp@users.noreply.github.com>
2024-04-24 23:51:37 +0700

    Add files via upload

---
+++ sait_db_uts.sql
@@ -0,0 +1,138 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 24, 2024 at 06:49 PM
+-- Server version: 10.4.28-MariaDB-log
+-- PHP Version: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `sait_db_uts`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mahasiswa`
+--
+
+CREATE TABLE `mahasiswa` (
+  `nim` varchar(10) NOT NULL,
+  `nama` varchar(20) NOT NULL,
+  `alamat` varchar(40) NOT NULL,
+  `tanggal_lahir` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `mahasiswa`
+--
+
+INSERT INTO `mahasiswa` (`nim`, `nama`, `alamat`, `tanggal_lahir`) VALUES
+('sv_001', 'joko', 'bantul', '1999-12-07'),
+('sv_002', 'paul', 'sleman', '2000-10-07'),
+('sv_003', 'andy', 'surabaya', '2000-02-09');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `matakuliah`
+--
+
+CREATE TABLE `matakuliah` (
+  `kode_mk` varchar(10) NOT NULL,
+  `nama_mk` varchar(21) NOT NULL,
+  `sks` int(2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `matakuliah`
+--
+
+INSERT INTO `matakuliah` (`kode_mk`, `nama_mk`, `sks`) VALUES
+('svpl_001', 'database', 2),
+('svpl_002', 'kecerdasan artifisial', 2),
+('svpl_003', 'interoperabilitas', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `perkuliahan`
+--
+
+CREATE TABLE `perkuliahan` (
+  `id_perkuliahan` int(5) NOT NULL,
+  `nim` varchar(10) NOT NULL,
+  `kode_mk` varchar(10) NOT NULL,
+  `nilai` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `perkuliahan`
+--
+
+INSERT INTO `perkuliahan` (`id_perkuliahan`, `nim`, `kode_mk`, `nilai`) VALUES
+(5, 'sv_002', 'svpl_002', 77),
+(12, 'sv_003', 'svpl_001', 77);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `mahasiswa`
+--
+ALTER TABLE `mahasiswa`
+  ADD PRIMARY KEY (`nim`);
+
+--
+-- Indexes for table `matakuliah`
+--
+ALTER TABLE `matakuliah`
+  ADD PRIMARY KEY (`kode_mk`);
+
+--
+-- Indexes for table `perkuliahan`
+--
+ALTER TABLE `perkuliahan`
+  ADD PRIMARY KEY (`id_perkuliahan`),
+  ADD KEY `nim` (`nim`),
+  ADD KEY `kode_mk` (`kode_mk`),
+  ADD KEY `nim_2` (`nim`,`kode_mk`),
+  ADD KEY `nim_3` (`nim`,`kode_mk`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `perkuliahan`
+--
+ALTER TABLE `perkuliahan`
+  MODIFY `id_perkuliahan` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `perkuliahan`
+--
+ALTER TABLE `perkuliahan`
+  ADD CONSTRAINT `perkuliahan_ibfk_2` FOREIGN KEY (`kode_mk`) REFERENCES `matakuliah` (`kode_mk`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `perkuliahan_ibfk_3` FOREIGN KEY (`nim`) REFERENCES `mahasiswa` (`nim`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
